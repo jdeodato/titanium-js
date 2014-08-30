a0b970e3206b68f5de843f88fb39fab0d0e10546
cribeiro <claudio.ribeiro@premium-minds.com>
2014-08-30 18:28:43 +0100

    added titanium helpers to deal with uuid's

---
+++ src/titanium.helpers.js
@@ -0,0 +1,24 @@
+Titanium.Helpers = function() {
+
+    return {
+        generateUUID: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0,
+                    v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        },
+
+        parseUUID: function(uuid) {
+            if (!_.isString(uuid) || _.isEmpty(uuid)) {
+                return false;
+            }
+
+            return result = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/.test(uuid);
+        },
+
+        getUTCFormat: function() {
+            return 'YYYY-MM-DDTHH:mm:ss';
+        }
+    }
+}();
