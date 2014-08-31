ebb104d691e39afa9013fe509290d34c17809ab1
cribeiro <claudio.ribeiro@premium-minds.com>
2014-08-31 02:06:36 +0100

    updated view and helpers

--- src/titanium.helpers.js
+++ src/titanium.helpers.js
@@ -14,7 +14,13 @@ Titanium.Helpers = Titanium.Helpers || function() {
                 return false;
             }
 
-            return result = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/.test(uuid);
+            var result = uuid.match(/[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/);
+
+            if(!result || result.length === 0){
+                return false;
+            }
+
+            return result[0];
         },
 
         getUTCFormat: function() {
--- src/titanium.view.js
+++ src/titanium.view.js
@@ -1,94 +1,96 @@
 Titanium.View = Titanium.View || Backbone.View.extend({
-	initialize: function(options){
-			this.subviews = {};
+    initialize: function(options) {
+        this._subviews = {};
 
-			this.overrideRenderMethod();
-			this.initializeView(options);
+        this.overrideRenderMethod();
+        this.initializeView(options);
 
-			return this;
-		},
+        return this;
+    },
 
-		initializeView: function(options){
-			//should be implemented on extended classes
-			return this;
-		},
+    initializeView: function(options) {
+        //should be implemented on extended classes
+        return this;
+    },
 
-		dispose: function(){
-			if(this.isDisposed){ return; }
+    dispose: function() {
+        if (this.isDisposed) {
+            return;
+        }
 
-			if(this.onDispose){
-				this.onDispose();
-			}
-			
-			this.isDisposed = true;
+        if (this.onDispose) {
+            this.onDispose();
+        }
 
-			this.disposeSubviews();
-            this.$el.empty();
-			this.stopListening();
-            this.unbind();
-            this.remove();
-		},
+        this.isDisposed = true;
 
-		onClose: function(){
-			//should be implemented on extended classes
-			return this;
-		},
+        this.disposeSubviews();
+        this.$el.empty();
+        this.stopListening();
+        this.unbind();
+        this.remove();
+    },
 
-		getSubviews: function(){
-			if(!this.subviews){
-				this.subviews = {};
-			}
+    onClose: function() {
+        //should be implemented on extended classes
+        return this;
+    },
 
-			return this.subviews;
-		},
+    getSubviews: function() {
+        if (!this._subviews) {
+            this._subviews = {};
+        }
 
-		addSubview: function(view){
-			if(view){
-				var viewCid = view.cid;
-				this.subviews[viewCid] = view;
-			}
+        return this._subviews;
+    },
 
-			return this;
-		},
+    addSubview: function(view) {
+        if (view) {
+            var viewCid = view.cid;
+            this._subviews[viewCid] = view;
+        }
 
-        getSubviewByCid: function(cid){
-            if(cid){
-                return this.subviews[cid];
+        return this;
+    },
+
+    getSubviewByCid: function(cid) {
+        if (cid) {
+            return this._subviews[cid];
+        }
+    },
+
+    disposeSubviews: function() {
+        for (var k in this._subviews) {
+            if (this._subviews[k]) {
+                view.dispose();
+                delete this._subviews[k];
             }
-        },
+        }
 
-		disposeSubviews: function(){
-			for(var k in this.subviews){
-				if(this.subviews[k]){
-					view.dispose();
-					delete this.subviews[k];
-				}
-			}
+        this._subviews = {};
 
-            this.subviews = {};
+        return this;
+    },
 
-			return this;
-		},
+    overrideRenderMethod: function() {
+        this.render = _.wrap(this.render, function(render) {
+            this.beforeRender();
+            render.apply(this);
+            this.afterRender();
 
-		overrideRenderMethod: function(){
-			this.render = _.wrap(this.render, function(render){
-				this.beforeRender();
-				render.apply(this);
-				this.afterRender();
+            return this;
+        });
 
-                return this;
-			});
+        return this;
+    },
 
-			return this;
-		},
+    beforeRender: function() {
+        //should be implemented on extended classes
+        return this;
+    },
 
-		beforeRender: function(){
-			//should be implemented on extended classes
-			return this;
-		},
-
-		afterRender: function(){
-			//should be implemented on extended classes
-			return this;
-		}
+    afterRender: function() {
+        //should be implemented on extended classes
+        return this;
+    }
 });
